81c31bb223848a287922f0479ea2b0d1007f09b8
Tomas Gear <tomas.e.gear@gmail.com>
2020-02-05 22:01:13 -0500

    add react-router-dom as dependency and serve up new homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage/index";
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Venue App </h1>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+         <Route exact path="/" component={HomePage}/>
+        </Switch>
+      </Router>
     );
   }
 }
