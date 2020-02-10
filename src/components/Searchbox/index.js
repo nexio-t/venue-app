2b31bc42ae847c4935b910ecde441f5a208e4aa4
Tomas Gear <tomas.e.gear@gmail.com>
2020-02-10 02:22:00 -0500

    refactor searchbox to be standalone bulma box with shadow

--- src/components/Searchbox/index.js
+++ src/components/Searchbox/index.js
@@ -3,34 +3,41 @@ import "./style.css";
 import "bulma/css/bulma.css";
 
 const Searchbox = props => {
-    return (
-        <div>
-            {/* {console.log(props)};   */}
-            <div className="parentDiv">Find a place</div>
-            <form>
-                <div className="field">
-                    <br></br>
-                    <input
-                        className="input"
-                        value={props.value}
-                        type="text"
-                        name="userSearch"
-                        placeholder="Urban Outfitters University City"
-                        onChange={props.handleInputChange}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="button is-dark"
-                    onClick={props.handleFormSubmit}
-                >
-                    Search
-        </button>
-            </form>
+  return (
+    <div>
+      {/* {console.log(props)};   */}
+      <div className="parentDiv">Find your place</div>
+      <form>
+        
 
-            
+        <div className="box">
+          <div className="field has-addons">
+            <div className="control is-expanded">
+              <input
+                className="input has-text-centered"
+                value={props.value}
+            type="text"
+            name="userSearch"
+                
+                placeholder="Philadelphia Museum of Art"
+                onChange={props.handleInputChange}
+              />
+            </div>
+            <div className="control">
+              <button type="submit" className="userSearchBtn button is-dark"  onClick={props.handleFormSubmit}>Search</button>
+            </div>
+          </div>
         </div>
-    );
+        {/* <button
+          type="submit"
+          className="button is-dark"
+          onClick={props.handleFormSubmit}
+        >
+          Search
+        </button> */}
+      </form>
+    </div>
+  );
 };
 
 export default Searchbox;
